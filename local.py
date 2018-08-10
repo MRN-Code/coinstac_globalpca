ad7bc3b58c3d97ba734a64890783013c76db0c7d
hvgazula <hvgazula@umich.edu>
2018-08-10 05:08:03 -0600

    Final working code

--- local.py
+++ local.py
@@ -1,158 +1,85 @@
 import json
 import sys
 import os
-import glob
 import numpy as np
-import scipy
+import scipy.sparse.linalg as splinalg
 from ancillary import list_recursive
 
-DEFAULT_k1 = 120
-DEFAULT_k2 = 500
-DEFAULT_data_file = 'data.txt'
+DEFAULT_k1 = 125
+DEFAULT_k2 = 200
+DEFAULT_data_file = 'data'
 
 
-def local_noop(**kwargs):
-    """
-        # Description:
-            local_preproc - preprocess data by centering
+def local_noop(args):
 
-        # PREVIOUS PHASE:
-            NA
-
-        # INPUT:
-
-        # OUTPUT:
-
-        # NEXT PHASE:
-            remote_init_env
-    """
-    computation_output = dict(
-        output=dict(
-            computation_phase="local_noop"
-        ),
-    )
+    computation_output = dict(output=dict(computation_phase="local_noop"), )
 
     return json.dumps(computation_output)
 
 
 def local_preprocess(args, data_file=DEFAULT_data_file, **kwargs):
-    """
-        # Description:
-            local_preprocess - preprocess data by centering
 
-        # PREVIOUS PHASE:
-            NA
+    data = np.load(os.path.join(args["state"]["baseDirectory"], data_file))
+    data = data[:, 0:500, :]
 
-        # INPUT:
+    data_mean = np.mean(data, 1)
+    data_mean = np.reshape(data_mean, (data.shape[0], 1, data.shape[2]))
 
-        # OUTPUT:
-
-        # NEXT PHASE:
-            remote_init_env
-    """
-
-    data = np.loadtxt(os.path.join(args["state"]["baseDirectory"], data_file))
-    data = data - np.matlib.repmat(np.mean(data, 0), 1, data.size[1])
+    data = data - np.tile(data_mean, (1, data.shape[1], 1))
 
     computation_output = dict(
-        output=dict(
-            computation_phase="local_preprocess"
-        ),
-        cache=dict(
-            all_data=data,
-        ),
-    )
+        output=dict(computation_phase="local_preprocess"),
+        cache=dict(all_data=data.tolist(), ), )
 
     return json.dumps(computation_output)
 
 
 def local_subject_pca(args, k1=DEFAULT_k1, **kwargs):
-    """
-        # Description:
-            local_pca - perform subject-level PCA
 
-        # PREVIOUS PHASE:
-            local_preprocess
+    all_data = np.array(args['cache']['all_data'])
+    red_data = np.zeros((all_data.shape[0], all_data.shape[1], k1))
 
-        # INPUT:
+    for subject in range(all_data.shape[0]):
+        data = all_data[subject, :, :]
+        w, v = splinalg.eigs(data.T.dot(data), k=k1)
+        red_data[subject, :, :] = data @ v
 
-        # OUTPUT:
-
-        # NEXT PHASE:
-            remote_init_env
-    """
-    all_data = args['cache']['all_data']
-    for row in range(all_data.shape[0]):
-        data = all_data[row, :]
-        w, v = scipy.sparse.linalg.eigs(data.dot(data.T), k=k1)
-        all_data[row, :] = data.dot(v)
     computation_output = dict(
-        output=dict(
-            computation_phase="local_noop"
-            ),
-        cache=dict(
-            all_data=all_data,
-        ),
-    )
-    
+        output=dict(computation_phase="local_subject_pca"),
+        cache=dict(all_data=red_data.tolist(), ), )
+
     return json.dumps(computation_output)
 
 
 def local_site_pca(args, k2=DEFAULT_k2, **kwargs):
-    """
-        # Description:
-            local_pca
 
-        # PREVIOUS PHASE:
-            NA
+    all_data = np.array(args['cache']['all_data'])
+    shape = all_data.shape
 
-        # INPUT:
-
-        # OUTPUT:
-
-        # NEXT PHASE:
-            remote_init_env
-    """
-    all_data = args['cache']['all_data']
     # Concatenate in the time dimension
-    w, v = scipy.sparse.linalg.eigs(data.T.dot(data), k=k2)
-    data = all_data.dot(v)
+    all_data = all_data.transpose(1, 0, 2).reshape(shape[1], -1)
+
+    w, v = splinalg.eigs(all_data.T.dot(all_data), k=k2)
+    red_data = all_data @ v
+    red_data = np.real(red_data)
+
     computation_output = dict(
-        output=dict(
-            computation_phase="local_noop"
-        ),
-        cache=dict(
-            data=data
-        ),
-    )
+        output=dict(computation_phase="local_site_pca"),
+        cache=dict(data=red_data.tolist(), ), )
 
     return json.dumps(computation_output)
 
 
-def local_return_data(args, flags=[], **kwargs):
-    """
-        # Description:
-            local_pca
+def local_return_data(args):
 
-        # PREVIOUS PHASE:
-            NA
-
-        # INPUT:
-
-        # OUTPUT:
-
-        # NEXT PHASE:
-            remote_init_env
-    """
     data = None
-    if args['state']['siteName'] in flags:
+    if args['state']['clientId'] == args['input']['flag']:
         data = args['cache']['data']
+
     computation_output = dict(
         output=dict(
-            computation_phase="local_noop",
-            data=data,
-        ),
-    )
+            computation_phase='local_return_data',
+            data=data, ), )
 
     return json.dumps(computation_output)
 
@@ -161,12 +88,16 @@ if __name__ == '__main__':
 
     parsed_args = json.loads(sys.stdin.read())
     phase_key = list(list_recursive(parsed_args, 'computation_phase'))
-    if not phase_key:  # FIRST PHASE
-        computation_output = local_preprocess(parsed_args, **parsed_args['input'])
-        computation_output = local_subject_pca(parsed_args, **json.loads(computation_output))
-        computation_output = local_site_pca(parsed_args, **json.loads(computation_output))
+
+    if not phase_key:
+        computation_output = local_noop(parsed_args)
+    elif 'remote_determine_site_order' in phase_key:
+        computation_output = local_preprocess(parsed_args)
+        computation_output = local_subject_pca(json.loads(computation_output))
+        computation_output = local_site_pca(json.loads(computation_output))
     elif 'remote_ping_sites' in phase_key:
-        computation_output = local_return_data(**parsed_args['input'])
+        computation_output = local_return_data(parsed_args)
     else:
         raise ValueError('Phase error occurred at LOCAL')
+
     sys.stdout.write(computation_output)
--- remote.py
+++ remote.py
@@ -1,96 +1,86 @@
 """
 Remote file for globalPCA
 """
-
-import os
+import collections
 import sys
 import json
 import numpy as np
+import scipy.sparse.linalg as splinalg
 from ancillary import list_recursive
 
-
-DEFAULT_data_file = 'data.txt'
+DEFAULT_data_file = 'data'
 DEFAULT_k3 = 100
 
 
-def remote_determine_site_order(args, **kwargs):
-    site_order = list(range(len(args['input'])))
-    site_flags = site_order[:]
-    current_site = 0
+def remote_determine_site_order(args):
+
+    site_order = list(args['input'].keys())
     np.random.shuffle(site_order)
+
     computation_output = dict(
-        output=dict(
-            computation_phase="remote_determine_site_order"
-        ),
-        cache=dict(
-            site_order=site_order,
-            site_flags=site_flags,
-            current_site=current_site,
-        ),
-        success=True
-    )
+        output=dict(computation_phase="remote_determine_site_order"),
+        cache=dict(site_order=site_order, ), )
     return json.dumps(computation_output)
 
 
 def remote_ping_sites(args, **kwargs):
-    current_site = args['cache']['current_site']
-    flag = args['cache']['site_flags'][current_site]
+
+    site_order = args["cache"]["site_order"]
+    site_order = collections.deque(site_order)
+    flag = site_order.popleft()
+
+    if not site_order:
+        conclude = 1
+    else:
+        conclude = 0
+
     computation_output = dict(
         output=dict(
             computation_phase="remote_ping_sites",
-            flag=flag
-        ),
-        success=True
-    )
-
-    return json.dumps(computation_output)
-
-
-def remote_check_finished(args, **kwargs):
-    current_site = args['cache']['current_site'] + 1
-    phase_key = 'remote_check_finished_false'
-    if current_site > len(args['input']):
-        phase_key = 'remote_check_finished_true'
-    computation_output = dict(
-        output=dict(
-            computation_phase=phase_key
-        ),
-        cache=dict(
-            current_site=current_site
-        ),
-        sucess=True,
+            flag=flag, ),
+        cache=dict(site_order=list(site_order), flag=flag, conclude=conclude),
     )
 
     return json.dumps(computation_output)
 
 
 def remote_pca_reduce(args, k3=DEFAULT_k3, **kwargs):
-    current_site = args['cache']['current_site']
-    received_data = args['input'][current_site]['data']
-    cached_data = args['cache']['data']
-    data = np.concatenate(cached_data, received_data)
-    w, v = scipy.linalg.eigs(data.T.dot(data), k=k3)
+
+    conclude = args['cache']['conclude']
+    flag = args['cache']['flag']
+    received_data = np.array(args['input'][flag]['data'])
+
+    try:
+        cached_data = np.array(args['cache']['data'])
+    except KeyError:
+        cached_data = []
+
+    try:
+        data = np.concatenate((cached_data, received_data), axis=1)
+    except ValueError:
+        data = received_data
+
+    w, v = splinalg.eigs(data.T.dot(data), k=k3)
     data = data.dot(v)
+    data = np.real(data)
+
     computation_output = dict(
-        output=dict(
-            computation_phase="remote_pca_reduce"
-        ),
-        cache=dict(
-            data=data
-        ),
-        success=True,
-    )
+        output=dict(computation_phase="remote_pca_reduce", conclude=conclude),
+        cache=dict(data=data.tolist()), )
 
     return json.dumps(computation_output)
 
 
-def remote_normalize_top_columns(args, **kwargs):
+def remote_normalize_top_columns(prev_output):
+
+    data = np.array(prev_output['cache']['data'])
+    data = data / np.linalg.norm(data, axis=1).reshape((-1, 1))
+
     computation_output = dict(
         output=dict(
-            computation_phase="remote_normalize_top_columns"
-        ),
-        success=True,
-    )
+            data=data.tolist(),
+            computation_phase="remote_normalize_top_columns"),
+        success=True, )
 
     return json.dumps(computation_output)
 
@@ -100,15 +90,19 @@ if __name__ == '__main__':
     parsed_args = json.loads(sys.stdin.read())
     phase_key = list(list_recursive(parsed_args, 'computation_phase'))
 
-    if 'local_noop' in phase_key:  # FIRST PHASE
+    if 'local_noop' in phase_key:
         computation_output = remote_determine_site_order(parsed_args)
-    elif 'local_site_pca' in phase_key or 'remote_check_finished_false' in phase_key:
-        computation_output = remote_ping_sites(parsed_args)  # First site ping
+    elif 'local_site_pca' in phase_key:
+        computation_output = remote_ping_sites(parsed_args)
     elif 'local_return_data' in phase_key:
         computation_output = remote_pca_reduce(parsed_args)
-        computation_output = remote_check_finished(parsed_args, json.loads(computation_output))
-        if computation_output['output']['computation_phase'] == 'remote_check_finished_true':
-            computation_output = remote_normalize_top_columns(parsed_args, json.loads(computation_output))
+        loaded_output = json.loads(computation_output)
+        if loaded_output['output']['conclude']:
+            computation_output = remote_normalize_top_columns(loaded_output)
+        else:
+            computation_output = remote_ping_sites(parsed_args)
+
     else:
         raise ValueError('Oops')
+
     sys.stdout.write(computation_output)
